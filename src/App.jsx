e6a5474cde233d45613fff18d2538c934fdbc284
VishalThakur04 <vishalthakur212311@gmail.com>
2025-09-28 00:06:22 +0530

    Fixed footer name convention in import

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
-import React from 'react';
 import Navbar from './components/Navbar';
-import Footer from './components/footer';
+import Footer from './components/Footer';
 import Home from './pages/Home';
 import { Route, Routes } from 'react-router-dom';
 import Register from './pages/Register';
